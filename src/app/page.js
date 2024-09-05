9304a6c9363fa68b7cd7cd2e1a5f64ecaac3eee0
Gutierre <gutigalmeida@hotmail.com>
2024-09-05 15:50:01 -0300

    Projeto Tela de Login

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,78 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { Component } from "react"
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button, Card, CardBody } from 'reactstrap';
+import { Icon } from '@mdi/react';
+import { mdiAccount } from '@mdi/js';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export class Login extends Component {  
+  
+  constructor() {
+        super();
+        this.state = {
+            showPassword: false,
+        };
+    }
+    toggleShowPassword = () => {
+        this.setState({ showPassword: !this.state.showPassword });
+    };
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    render() {
+        return (
+            <Container className="d-flex justify-content-center align-items-center min-vh-100">
+                <Row>
+                    <Col md="6">
+                        <Card style={{width: '600px' }}>
+                            <CardBody>
+                                <div className="text-center mb-4">
+                                    <Icon
+                                        path={mdiAccount}
+                                        title="User Profile"
+                                        size={5}
+                                        horizontal
+                                        vertical
+                                        rotate={180}
+                                        color="black"
+                                    />
+                                </div>
+                                <Form>
+                                    <FormGroup>
+                                        <Label for="login">LOGIN</Label>
+                                        <Input
+                                            type="text"
+                                            id="login"
+                                            placeholder="Digite seu login"
+                                        />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="senha">SENHA</Label>
+                                        <div className="d-flex">
+                                            <Input
+                                                type={this.state.showPassword ? "text" : "password"}
+                                                placeholder="Digite sua senha"
+                                            />
+                                            <Button
+                                                type="button"
+                                                color="secondary"
+                                                onClick={this.toggleShowPassword}
+                                                style={{ marginLeft: '10px' }}
+                                            >
+                                                {this.state.showPassword ? <FaEyeSlash /> : <FaEye />}
+                                            </Button>
+                                        </div>
+                                    </FormGroup>
+                                    <div className="text-center mb-3">
+                                        <a href="#">ESQUECI A SENHA</a>
+                                    </div>
+                                    <Button type="submit" color="primary" block>ENTRAR</Button>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
+
+export default Login;
